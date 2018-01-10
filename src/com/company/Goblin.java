b337f7864635692c1b1812beb4d1f3f3902bd266
Unknown <vincent.brebion@etu.univ-nantes.fr>
2018-01-10 16:13:26 +0100

    Ajout du javafx

---
+++ src/com/company/Goblin.java
@@ -0,0 +1,89 @@
+package com.company;
+
+import com.company.fxcomponent.Hexagon;
+import com.company.system.Triplet;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.awt.geom.Point2D;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.StrictMath.sqrt;
+
+public class Goblin extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+
+			Scene scene = new Scene(root, 1080, 800);
+			ScrollPane scrollPane = (ScrollPane) scene.lookup("#scrollPane");
+
+			List<Hexagon> hexagons = new ArrayList<>();
+
+			/*float height = (float) sqrt(3) * 30;
+			float distHorizontal = 30 * 1.5f;
+
+			for (int col = 0; col < (640 / distHorizontal); col++) {
+				for (int row = 0; row < (640 / height); row++) {
+					int x = col - 16;
+					int z = row - (col - (col & 1)) / 2;
+
+					hexagons.add(
+						new Hexagon(
+							new Point2D.Double(
+								distHorizontal * col,
+								height * (row + ((col%2 == 0) ? 0 : 0.5f))),
+							30,
+							new Triplet(x, - x - z, z),
+							Hexagon.FLAT)
+					);
+				}
+			}*/
+
+			float width = (float) sqrt(3) * 30;
+			float distVertical = 30 * 1.5f;
+
+			float colNumber = 1080 / width;
+			float rowNumber = 80 / distVertical;
+
+			for (int col = 0; col < colNumber; col++) {
+				for (int row = 0; row < rowNumber; row++) {
+					int x = col - (row - (row & 1)) / 2 - 9;
+					int z = row - 9;
+
+					hexagons.add(
+						new Hexagon(
+							new Point2D.Double(
+									width * (col + ((row % 2 == 0) ? 0 : 0.5f)),
+									distVertical * row),
+							30,
+							new Triplet(x, - x - z, z),
+							Hexagon.POINTY)
+					);
+				}
+			}
+
+			Pane pane = new Pane(hexagons.toArray(new Hexagon[0]));
+
+			scrollPane.setContent(pane);
+
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
