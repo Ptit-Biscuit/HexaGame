88478787e447a53644ea2c7446834ad11ea2acee
Unknown <vincent.brebion@etu.univ-nantes.fr>
2018-01-16 10:37:06 +0100

    refactorisation gestion des unités

--- src/com/company/model/managers/GraphicManager.java
+++ src/com/company/model/managers/GraphicManager.java
@@ -1,4 +1,69 @@
 package com.company.model.managers;
 
-public class GraphicManager {
+import com.company.model.TileType;
+import org.apache.logging.log4j.LogManager;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Manager of the tiles
+ */
+public abstract class GraphicManager {
+	/**
+	 * Tileset containing all the tiles
+	 */
+	private HashMap<TileType, BufferedImage> tileset = new HashMap<>(); // (Tilename, Tile)
+
+	/**
+	 * Adding a tile to the tileset
+	 * @param name The name of the tile
+	 * @param tile The tile
+	 */
+	public void addTile(TileType name, BufferedImage tile){
+		tileset.put(name, tile);
+	}
+
+	/**
+	 * Getter of a tile
+	 * @param name The name of the tile
+	 * @return The tile if found by name, null otherwise
+	 */
+	public BufferedImage getTile(TileType name){ return tileset.get(name); }
+
+	/**
+	 * Parse a tileset in a file and initialize all the tiles
+	 * @param tilesetFile The file containing the tileset
+	 * @param width The width of a tile
+	 * @param names The names of the tiles
+	 */
+	public void parsePicture(File tilesetFile, int width, ArrayList<TileType> names) { parsePicture(tilesetFile, width, width, names); }
+
+	/**
+	 * Parse a tileset in a file and initialize all the tiles
+	 * @param tilesetFile The file containing the tileset
+	 * @param width The width of a tile
+	 * @param height The height of a tile
+	 * @param names The names of the tiles
+	 */
+	public void parsePicture(File tilesetFile, int width, int height, ArrayList<TileType> names){
+		try {
+			BufferedImage tilemap = ImageIO.read(tilesetFile);
+			int xmax = tilemap.getWidth() / width;
+			int ymax = tilemap.getHeight() / height;
+
+			for (int x = 0; x < xmax; ++x)
+				for (int y = 0; y < ymax; ++y) {
+					TileType name = names.get(y + x * ymax);
+					if(name != null)
+						tileset.put(name, tilemap.getSubimage(x * width, y * height, width, height));
+				}
+		} catch (IOException e) {
+			LogManager.getLogger(GraphicManager.class).error(e.getMessage());
+		}
+	}
 }
--- src/com/company/model/managers/TileManager.java
+++ src/com/company/model/managers/TileManager.java
@@ -1,20 +1,11 @@
-package com.company.model;
-
-import org.apache.logging.log4j.LogManager;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
+package com.company.model.managers;
 
 /**
  * Manager of the tiles
  */
-public class TileManager {
+public class TileManager extends GraphicManager {
 	/**
-	 * Unique intance of TileManager (Singleton Pattern)
+	 * Unique instance of TileManager (Singleton Pattern)
 	 */
     private static TileManager instance = new TileManager();
 
@@ -22,59 +13,6 @@ public class TileManager {
 	 * Default constructor
 	 */
 	private TileManager() {  }
-
-	/**
-	 * Tileset containing all the tiles
-	 */
-	private HashMap<TileType, BufferedImage> tileset = new HashMap<>(); // (Tilename, Tile)
-
-	/**
-	 * Adding a tile to the tileset
-	 * @param name The name of the tile
-	 * @param tile The tile
-	 */
-    public void addTile(TileType name, BufferedImage tile){
-        tileset.put(name, tile);
-    }
-
-	/**
-	 * Getter of a tile
-	 * @param name The name of the tile
-	 * @return The tile if found by name, null otherwise
-	 */
-	public BufferedImage getTile(TileType name){ return tileset.get(name); }
-
-	/**
-	 * Parse a tileset in a file and initialize all the tiles
-	 * @param tilesetFile The file containing the tileset
-	 * @param width The width of a tile
-	 * @param names The names of the tiles
-	 */
-	public void parsePicture(File tilesetFile, int width, ArrayList<TileType> names) { parsePicture(tilesetFile, width, width, names); }
-
-	/**
-	 * Parse a tileset in a file and initialize all the tiles
-	 * @param tilesetFile The file containing the tileset
-	 * @param width The width of a tile
-	 * @param height The height of a tile
-	 * @param names The names of the tiles
-	 */
-    public void parsePicture(File tilesetFile, int width, int height, ArrayList<TileType> names){
-        try {
-            BufferedImage tilemap = ImageIO.read(tilesetFile);
-            int xmax = tilemap.getWidth() / width;
-            int ymax = tilemap.getHeight() / height;
-
-            for (int x = 0; x < xmax; ++x)
-            	for (int y = 0; y < ymax; ++y) {
-					TileType name = names.get(y + x * ymax);
-                	if(name != null)
-	                    tileset.put(name, tilemap.getSubimage(x * width, y * height, width, height));
-	            }
-        } catch (IOException e) {
-	        LogManager.getLogger(TileManager.class).error(e.getMessage());
-        }
-    }
 
 	/**
 	 * Complementary method of singleton pattern
