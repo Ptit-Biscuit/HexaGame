29f0f0ccd313acbde88c275b807e034dc2c3417b
Unknown <vincent.brebion@etu.univ-nantes.fr>
2018-02-07 20:13:52 +0100

    check s'il y a une armée

--- src/com/company/model/actions/Movement.java
+++ src/com/company/model/actions/Movement.java
@@ -24,6 +24,10 @@ public class Movement {
 	 */
 	public static Boolean isValidMove(Army army, ArrayList<Tile> tileList) {
 		int mpCost = 1;
+
+		if (army == null)
+			return false;
+
 		army.setGhostPosition(army.getPosition());
 		army.setGhostMP(army.getMP());
 		for (Tile tile:tileList) {
